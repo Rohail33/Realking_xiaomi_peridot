2cf92ac8b8eb912b137f53800751ccdfed16e260
DevUknown <vidium400@gmail.com>
2024-06-02 15:27:51 +0200

    KERNEL: blacklist zram and zsmalloc for compiling inline

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2301,13 +2301,17 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
+static char *custom_module_blacklist[] = {
+	"zram","zsmalloc"
+};
 static bool blacklisted(const char *module_name)
 {
 	const char *p;
 	size_t len;
+	int i;
 
 	if (!module_blacklist)
-		return false;
+		goto custom_blacklist;
 
 	for (p = module_blacklist; *p; p += len) {
 		len = strcspn(p, ",");
@@ -2316,6 +2320,10 @@ static bool blacklisted(const char *module_name)
 		if (p[len] == ',')
 			len++;
 	}
+	custom_blacklist:
+	for (i = 0; i < ARRAY_SIZE(custom_module_blacklist); i++)
+		if (!strcmp(module_name, custom_module_blacklist[i]))
+			return true;
 	return false;
 }
 core_param(module_blacklist, module_blacklist, charp, 0400);
@@ -2784,7 +2792,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	 * if it's blacklisted.
 	 */
 	if (blacklisted(info->name)) {
-		err = -EPERM;
+		//err = -EPERM;
 		pr_err("Module %s is blacklisted\n", info->name);
 		goto free_copy;
 	}
